de17ecb16ef1b0f444c254eaec3f7a5fef9e6233
sabbasbraimah <braimah_abbas@yahoo.co.uk>
2021-09-25 21:39:38 +0100

    first commit of default files

---
+++ src/main/java/com/sakays/com/demoweb/DemowebApplication.java
@@ -0,0 +1,13 @@
+package com.sakays.com.demoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebApplication.class, args);
+	}
+
+}
